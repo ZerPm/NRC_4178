8e639a7041deeb833cb473566332c4f3163814c5
Christian Paul Manco Malasquez <christian.manco3@gmail.com>
2025-11-29 13:58:45 +0000

    ok

--- Inicio Sesion/login.js
+++ Inicio Sesion/login.js
@@ -11,7 +11,7 @@ function validarLogin(event) {
   if (passwordInput.toLowerCase() === CLAVE_SECRETA) {
     alert("¡Acceso concedido! Bienvenido.");
     // Redirige a tu página principal
-    window.location.href = "landingpage.html";
+    window.location.href = "../LandingPage/landing.html";
   } else {
     alert("Clave incorrecta. Inténtalo de nuevo.");
     document.getElementById("password").value = "";
